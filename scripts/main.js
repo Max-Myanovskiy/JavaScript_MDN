566247ae0583a6b95bb94fad24a895109746a275
Max-Myanovskiy <myanovskiy.m@gmail.com>
2024-05-27 14:00:17 +0300

    update commit

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,13 @@
-var myHeading = document.querySelector("h1");
-myHeading.textContent = "Hello World!";
+// document.querySelector("h1").textContent = "MAX";
+
+var myImage = document.querySelector("img");
+
+myImage.onclick = function () {
+  var mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/firefox-icon.png") {
+    myImage.setAttribute("src", "images/firefox2.png");
+  } else {
+    myImage.setAttribute("src", "images/firefox-icon.png");
+  }
+};
+
