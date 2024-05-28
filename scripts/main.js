8c30e460eedceea49f27b5ffd6697148b85cad67
Max-Myanovskiy <myanovskiy.m@gmail.com>
2024-05-28 08:16:25 +0300

    button feature

--- scripts/main.js
+++ scripts/main.js
@@ -1,32 +1,8 @@
-// document.querySelector("h1").textContent = "MAX";
+const button = document.querySelector("button");
 
-var myImage = document.querySelector("img");
+button.addEventListener("click", updateName);
 
-myImage.onclick = function () {
-  var mySrc = myImage.getAttribute("src");
-  if (mySrc === "images/firefox-icon.png") {
-    myImage.setAttribute("src", "images/firefox2.png");
-  } else {
-    myImage.setAttribute("src", "images/firefox-icon.png");
-  }
-};
-
-var myButton = document.querySelector("button");
-var myHeading = document.querySelector("h1");
-
-function setUserName() {
-  var myName = prompt("Please enter your name.");
-  localStorage.setItem("name", myName);
-  myHeading.textContent = "Mozilla is cool, " + myName;
+function updateName() {
+  const name = prompt("Enter a new name");
+  button.textContent = `Player 1: ${name}`;
 }
-
-if (!localStorage.getItem("name")) {
-  setUserName();
-} else {
-  var storedName = localStorage.getItem("name");
-  myHeading.textContent = "Mozilla is cool, " + storedName;
-}
-
-myButton.onclick = function () {
-  setUserName();
-};
