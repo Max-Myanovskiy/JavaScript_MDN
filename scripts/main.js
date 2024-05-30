3ae817b13a15e36d58f18be64002c773d631e10f
Max-Myanovskiy <myanovskiy.m@gmail.com>
2024-05-30 13:39:54 +0300

    commit 1

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,9 @@
 const string = "Революция не будет транслироваться по телевидению.";
 string;
+
+const button = document.querySelector("button");
+
+button.onclick = function () {
+  const name = prompt("Как тебя зовут?");
+  alert("Привет, " + name + ", рад тебя видеть!");
+};
