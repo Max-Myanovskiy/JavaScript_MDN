c3d569da520f8d394d1eb95b73fd300d068752c8
Max-Myanovskiy <myanovskiy.m@gmail.com>
2024-05-30 11:20:50 +0300

    commit 4

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,32 @@
-const button = document.querySelector("button");
+var myInt = 5;
+var myFloat = 6.667;
+myInt;
+myFloat;
 
-button.onclick = function () {
-  let name = prompt("Как тебя зовут?");
-  alert("Привет " + name + ", рады вас видеть !");
-};
+10 + 7;
+9 * 8;
+60 % 3;
 
-// list = document.createElement("ul");
-// document.body.appendChild(list);
+var num1 = 10;
+var num2 = 50;
+9 * num1;
+num2 / num1;
+
+5 + 10 * 3;
+(num2 % 9) * num1;
+num2 + num1 / 8 + 2;
+
+var btn = document.querySelector("button");
+var txt = document.querySelector("p");
+
+btn.addEventListener("click", updateBtn);
+
+function updateBtn() {
+  if (btn.textContent === "Start machine") {
+    btn.textContent = "Stop machine";
+    txt.textContent = "The machine has started!";
+  } else {
+    btn.textContent = "Start machine";
+    txt.textContent = "The machine is stopped.";
+  }
+}
