081a0b30485351ea95410c33f3ced6e8e19d71d0
Max-Myanovskiy <myanovskiy.m@gmail.com>
2024-06-11 12:58:10 +0300

    commit 1

--- main.js
+++ main.js
@@ -1,14 +1,19 @@
-var displayedImage = document.querySelector('.displayed-img');
-var thumbBar = document.querySelector('.thumb-bar');
+var displayedImage = document.querySelector(".displayed-img");
+var thumbBar = document.querySelector(".thumb-bar");
 
-btn = document.querySelector('button');
-var overlay = document.querySelector('.overlay');
+btn = document.querySelector("button");
+var overlay = document.querySelector(".overlay");
 
 /* Looping through images */
 
-  var newImage = document.createElement('img');
-  newImage.setAttribute('src', xxx);
-  thumbBar.appendChild(newImage);
+var newImage = document.createElement("img");
+
+for (i = 0, i <= 4, i+=) {
+
+}
+
+newImage.setAttribute("src", "images/pic2.jpg");
+
+thumbBar.appendChild(newImage);
 
 /* Wiring up the Darken/Lighten button */
-
