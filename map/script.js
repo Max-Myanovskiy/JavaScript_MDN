0fe18f7dc6c3c3aaaf762c993fdb5d74a272ea07
Max-Myanovskiy <myanovskiy.m@gmail.com>
2024-06-27 13:40:52 +0300

    commit 2

---
+++ map/script.js
@@ -0,0 +1,21 @@
+// L.mapquest.key = "AIzaSyDDuGt0E5IEGkcE6ZfrKfUtE9Ko_de66pA";
+
+// // 'map' refers to a <div> element with the ID map
+// const map = L.mapquest.map("map", {
+//   center: [53.480759, -2.242631],
+//   layers: L.mapquest.tileLayer("map"),
+//   zoom: 12,
+// });
+
+var latlng = new google.maps.LatLng(
+  position.coords.latitude,
+  position.coords.longitude
+);
+var myOptions = {
+  zoom: 8,
+  center: latlng,
+  mapTypeId: google.maps.MapTypeId.TERRAIN,
+  disableDefaultUI: true,
+};
+
+var map = new google.maps.Map(document.getElementById("map"), myOptions);
